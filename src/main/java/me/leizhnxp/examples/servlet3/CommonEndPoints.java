1fc00bef68216952c321a773e441e0569c7b5d5a
leizhnxp <leizhnxp@gmail.com>
2015-11-08 08:00:05 +0800

    add servlet class and minimal web.xml

---
+++ src/main/java/me/leizhnxp/examples/servlet3/CommonEndPoints.java
@@ -0,0 +1,25 @@
+package me.leizhnxp.examples.servlet3;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/demo")
+public class CommonEndPoints extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+		resp.getWriter().println(req.getParameter("para"));
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6197062728780369381L;
+
+}
